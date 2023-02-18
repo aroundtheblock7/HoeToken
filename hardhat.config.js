1e65dfce27b2517e57407c3392703064a43ccd0e
Joseph DeAngeloi <josephdeangeloi@Josephs-MBP.home>
2023-02-18 12:04:29 -0500

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,9 +5,17 @@ require('dotenv').config()
 module.exports = {
   solidity: '0.8.17',
   networks: {
+    hardhat: {
+      chainId: 31337,
+  },
+    localhost: {
+      chainId: 31337,
+  },
     goerli: {
+      chainId: 5,
       url: process.env.ALCHEMY_GOERLI_URL,
-      accounts: [process.env.GOERLI_PRIVATE_KEY]
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+      saveDeployments: true,
     }
   },
   etherscan: {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,8 @@
+const { ethers, run, network } = require("hardhat")
+
 async function main() {
+  const chainId = network.config.chainId;
+
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
@@ -8,9 +12,29 @@ async function main() {
   console.log("Account balance:", (await ethers.utils.formatEther(weiAmount)));
 
   const Token = await ethers.getContractFactory("HoeToken");
-  const token = await Token.deploy();
 
-  console.log("Token address:", token.address);
+  //const args = [];
+  
+  console.log(`Deploying...`);
+  const token = await Token.deploy();
+  await token.deployed();
+  console.log(`Deployed!`);
+  console.log("HoeToken address:", token.address);
+
+
+  // * only verify on testnets or mainnets.
+  if (chainId != 31337 && process.env.ETHERSCAN_KEY) {
+    console.log(`Waiting for blocks confirmations...`);
+    await token.deployTransaction.wait(6);
+    console.log(`Confirmed!`);
+    //await verify(token.address, args);
+  }
+     
+  await hre.run("verify:verify", {
+    address: token.address,
+    contract: "contracts/HoeToken.sol:HoeToken", 
+    constructorArguments: [],
+ });
 }
 
 main()
